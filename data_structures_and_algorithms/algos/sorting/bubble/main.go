fa59e38365c8df3c94309bc3840e385d393b1876
JulianToledano <jtoledanodiaz@gmail.com>
2020-09-14 20:45:45 +0200

    Simple bubblesort
    Learning pointers in golang

---
+++ data_structures_and_algorithms/algos/sorting/bubble/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type arr struct {
+	size     int
+	contents []int
+}
+
+func main() {
+	a := newArr(50)
+	fmt.Printf("%T\n", &a.contents[1])
+	fmt.Println(a)
+	a.bubbleSort()
+	fmt.Println(a)
+}
+
+func newArr(size int) *arr {
+	rand.Seed(time.Now().Unix())
+	a := new(arr)
+	a.size = size
+	a.contents = rand.Perm(size)
+	return a
+}
+
+func (a *arr) bubbleSort() {
+	for i := 0; i < a.size; i++ {
+
+		for j := 0; j < (a.size - i - 1); j++ {
+			if a.contents[j] > a.contents[j+1] {
+				swap(&a.contents[j], &a.contents[j+1])
+			}
+		}
+	}
+}
+
+func swap(a *int, b *int) {
+	t := *a
+	*a = *b
+	*b = t
+}
